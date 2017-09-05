5b9784f2d6fdb7690f5f8436e5e24048704f6d3d
Ismael <ismaproco@gmail.com>
2017-09-05 23:44:38 +0200

    added compression build

--- Apps/webpack.config.js
+++ Apps/webpack.config.js
@@ -1,22 +1,33 @@
 const path = require("path");
 const webpack = require("webpack");
 const BabiliPlugin = require("babili-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 function getPlugins(env) {
 
     const PRODUCTION = env && env.PROD;
-    
+
     console.log('----> is production', PRODUCTION);
 
     return PRODUCTION ? [new BabiliPlugin({
-        removeDebugger: true,
-        mangle: { 'topLevel': true },
-        deadcode: true
-    }, {
+            removeDebugger: true,
+            mangle: {
+                'topLevel': true
+            },
+            deadcode: true
+        }, {
             comments: false
-        })] : [];
-    
-}    
+        }),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.(js|html)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ] : [];
+
+}
 
 module.exports = function (env) {
     return {
@@ -30,11 +41,16 @@ module.exports = function (env) {
         },
         plugins: getPlugins(env),
         module: {
-            rules: [
-                {
+            rules: [{
                     test: /\.css$/,
-                    use: [
-                        { loader: 'style-loader', options: { attrs: { id: 'id' } } },
+                    use: [{
+                            loader: 'style-loader',
+                            options: {
+                                attrs: {
+                                    id: 'id'
+                                }
+                            }
+                        },
                         {
                             loader: 'css-loader'
                         }
@@ -60,8 +76,20 @@ module.exports = function (env) {
                     test: /\.json$/,
                     loader: 'json-loader'
                 },
-                { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff", options: { outputPath: "../Views/" } },
-                { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader", options: { outputPath: "../Views/" } }
+                {
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: "url-loader?limit=10000&mimetype=application/font-woff",
+                    options: {
+                        outputPath: "../Views/"
+                    }
+                },
+                {
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "../Views/"
+                    }
+                }
 
             ]
         }
