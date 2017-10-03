77bfdd7c1858fc864e52b88606515c26295b4342
Ismael <ismaproco@gmail.com>
2017-10-03 18:03:38 +0200

    Removed unused files

--- Js/logIn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {    
-
-    $("#enter").on("click", function () {
-        window.location.href = "inicio.html";
-    })
-     //aqui va la llamada al backend para validar el usuario        
-  
-   
-});
